847c9e8709b8e4daad5af2f2ec25ae1dcff1095a
Matt Busigin <mbusigin@citrisoft.com>
2023-04-07 21:51:38 -0400

    Some stuff

---
+++ src/validator.ts
@@ -0,0 +1,7 @@
+import { GolemFile } from './types';
+import { GolemFileError } from './errors';
+
+export function validateGolemFile(golemFile: GolemFile): void {
+  // Validate the Golem file structure and content
+  // If any errors are found, throw a GolemFileError with a specific error message
+}
